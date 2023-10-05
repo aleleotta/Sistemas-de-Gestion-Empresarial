c2d25b113b8707fb61b0c4b67d12e2896c5c83cc
aleleotta <114069815+aleleotta@users.noreply.github.com>
2023-10-05 14:44:26 +0200

    Initial commit

---
+++ HelloWorld_WebForms/HelloWorld_WebForms/Default.aspx
@@ -0,0 +1,15 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="HelloWorld_WebForms._Default" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+
+    <main>
+        <h1>Hello World!</h1>
+    </main>
+
+    <asp:TextBox ID="txtNombre" runat="server" Height="31px" OnTextChanged="TextBox1_TextChanged" Width="175px"></asp:TextBox>
+    <asp:Label ID="lblSaludo" runat="server" Text="Label" Visible="False"></asp:Label>
+    <br />
+    <br />
+    <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Button" />
+
+</asp:Content>
---
+++ HelloWorld_WebForms/HelloWorld_WebForms/Default.aspx.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace HelloWorld_WebForms
+{
+    /// <summary>
+    /// Elements from main page
+    /// </summary>
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            if (!String.IsNullOrEmpty(txtNombre.Text)) {
+                lblSaludo.ForeColor = System.Drawing.Color.Black;
+                lblSaludo.Font.Bold = false;
+                lblSaludo.Text = "Hello " + txtNombre.Text + "!";
+                lblSaludo.Visible = true;
+            } else
+            {
+                lblSaludo.Text = "The name field cannot be empty!";
+                lblSaludo.ForeColor = System.Drawing.Color.Red;
+                lblSaludo.Font.Bold = true;
+                lblSaludo.Visible = true;
+            }
+        }
+
+    }
+}
