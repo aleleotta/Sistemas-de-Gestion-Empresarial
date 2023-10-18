60bc1e21a8aaada33c2dcf5e8e66119131d20f45
aleleotta <114069815+aleleotta@users.noreply.github.com>
2023-10-18 14:23:19 +0200

    .

---
+++ Unidad6_Ejercicio1/Unidad6_Ejercicio1/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Unidad6_Ejercicio1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public String Index()
+        {
+            return "Alessandro";
+        }
+
+        public String lastName()
+        {
+            return "Leotta";
+        }
+
+        public IActionResult Hello()
+        {
+            return View();
+        }
+
+    }
+}
